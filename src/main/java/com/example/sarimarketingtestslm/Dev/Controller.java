74ef2cdd783eb536f11a9203be51953c4acd54e2
ic20b501 <ic20b501@technikum-wien.at>
2022-12-21 19:04:17 +0100

    Issue2 Die Funktionalität fertig und somit auch die Anwendung "abgebbar" also halt so dass auf die prozentwerte der notenwert retouniert wird

--- src/main/java/com/example/sarimarketingtestslm/Dev/Controller.java
+++ src/main/java/com/example/sarimarketingtestslm/Dev/Controller.java
@@ -9,9 +9,19 @@ public class Controller {
     @GetMapping("/api")
     public String GetGrade(@RequestParam int percentage)
     {
-        if (percentage <= 80)
+        if (percentage >= 88)
             return "1";
+        if ((percentage >= 75) && (percentage < 88))
+            return "2";
+        if ((percentage >= 63) && (percentage < 75))
+            return "3";
+        if ((percentage >= 50) && (percentage < 63))
+            return "4";
+        if (percentage < 50)
+            return "5";
+        if (percentage > 100)
+            return "error wrong input value | please make a input between 0-100 as this is in percent";
         else
-            return "exception";
+            return "error wrong input value | please make a input between 0-100 as this is in percent";
     }
 }
