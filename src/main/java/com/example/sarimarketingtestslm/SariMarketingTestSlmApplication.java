58a743c12e068761a0e3797edd728fabaf910877
ic20b501 <ic20b501@technikum-wien.at>
2022-12-21 18:07:34 +0100

    Initial commit

---
+++ src/main/java/com/example/sarimarketingtestslm/SariMarketingTestSlmApplication.java
@@ -0,0 +1,13 @@
+package com.example.sarimarketingtestslm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SariMarketingTestSlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SariMarketingTestSlmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sarimarketingtestslm/SariMarketingTestSlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sarimarketingtestslm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SariMarketingTestSlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
